98faa99b6b2f8f4755ae218d2c0f37ddf64ba6a8
jtr354 <jtr354@foxmail.com>
2022-04-15 20:37:33 +0800

    feat:modify carports

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import time
 import pyautogui
 
 if __name__ == "__main__":
     pyautogui.FAILSAFE = False
     count = 0
-    while count < 1000:
+    while True:
+        if count > 100:
+            break
         print(count)
         count = count + 1
-        pyautogui.click()
-        # time.sleep(0)
+        pyautogui.click(clicks=20, interval=0)
