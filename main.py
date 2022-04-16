9500e3fd6f43681dac196eaaba37de5a79a3dfd4
jtr354 <jtr354@foxmail.com>
2022-04-16 08:46:53 +0800

    feat:  carport modify

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     pyautogui.FAILSAFE = False
     count = 0
     while True:
-        if count > 100:
-            break
-        print(count)
+        # if count > 1e20:
+        #     break
+        # print(count)
         count = count + 1
-        pyautogui.click(clicks=20, interval=0)
+        pyautogui.click(clicks=100, interval=0)
--- packages/antd-demo/src/pages/carport/index.jsx
+++ packages/antd-demo/src/pages/carport/index.jsx
@@ -1,13 +1,26 @@
-import React, { useMemo, useReducer, useState } from "react";
-import { Button, Drawer } from "antd";
+import React, { useMemo, useReducer, useState, useEffect } from "react";
+import { Button, Drawer, message } from "antd";
 import "./styles.less";
 
 import "antd/dist/antd.less";
+
+let current = 1;
+let timer;
+let lock = false;
 const Carport = () => {
   const [count, setCount] = useReducer((c) => c + 1, 0);
   const toggle = useMemo(() => {
     return count % 2 === 0;
   }, [count]);
+  const [loading, setLoading] = useState(false);
+  const [reset, setReset] = useState(false);
+  useEffect(() => {
+    if (reset) {
+      current = 1;
+      lock = false;
+      setReset(false);
+    }
+  }, [reset]);
   return (
     <div className="App">
       <Drawer
@@ -27,7 +40,39 @@ const Carport = () => {
           <div className="button-wrapper">
             {toggle && <div className="heart" />}
             <Button
-              onClick={setCount}
+              onClick={() => {
+                // if (isLock.current) return;
+                console.log(current);
+                if (current && current % 2 === 0) {
+                  if (lock) return;
+                  setLoading(true);
+                  clearTimeout(timer);
+                  lock = true;
+                  timer = timeBack(10, () => {
+                    setLoading(false);
+                    setReset(true);
+                  });
+                } else {
+                  setLoading(false);
+                  current++;
+                }
+                setCount();
+
+                function timeBack(time, cb) {
+                  if (time === 0) {
+                    cb && cb();
+                    return "continue";
+                  }
+                  // console.warn(`倒计时${time}`);
+                  const hide = message.loading(`倒计时${time}`);
+                  setTimeout(() => {
+                    time--;
+                    hide();
+                    return timeBack(time, cb);
+                  }, 1000);
+                }
+              }}
+              loading={loading}
               className="button"
               type="primary"
               size="large"
