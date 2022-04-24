8370e0a43219b0e7ad62a8436df1d9aa4fe1ca55
jtr354 <jtr354@foxmail.com>
2022-04-25 07:20:32 +0800

    feat:performace api

--- packages/performace-api/src/pages/FormDemo/index.jsx
+++ packages/performace-api/src/pages/FormDemo/index.jsx
@@ -4,30 +4,30 @@ import { createContext } from "react";
 import { useState, memo, useRef } from "react";
 import React from "react";
 
-// const FormContext = createContext();
-// const SetFormContext = createContext();
+const FormContext = createContext();
+const SetFormContext = createContext();
 
 const FieldContext = createContext();
 const FieldUpdateContext = createContext();
 
-const Form = ({ children, initialValues, form: formRef }) => {
-  const [form, setForm] = useState(initialValues);
-  formRef.current = form;
-  // return (
-  //   <SetFormContext.Provider value={setForm}>
-  //     <FormContext.Provider value={form}>
-  //       {/* <Middle>{children}</Middle> */}
-  //       {children}
-  //     </FormContext.Provider>
-  //   </SetFormContext.Provider>
-  // );
-  return React.Children.map(children, (child) => {
-    if (React.isValidElement(child)) {
-      return React.cloneElement(child, { form, setForm });
-    }
-    console.log(child);
-    return child;
-  });
+const Form = ({ children, form, setForm }) => {
+  // const [form, setForm] = useState(initialValues);
+  // formRef.current = form;
+  return (
+    <SetFormContext.Provider value={setForm}>
+      <FormContext.Provider value={form}>
+        {/* <Middle>{children}</Middle> */}
+        {children}
+      </FormContext.Provider>
+    </SetFormContext.Provider>
+  );
+  // return React.Children.map(children, (child) => {
+  //   if (React.isValidElement(child)) {
+  //     return React.cloneElement(child, { form, setForm });
+  //   }
+  //   console.log(child);
+  //   return child;
+  // });
 };
 
 // const Middle = memo(({ children }) => {
@@ -40,30 +40,46 @@ function useForm() {
 }
 
 const FormDemo = () => {
-  const form = useForm();
+  // const form = useForm();
+  const [form, setForm] = useState({});
+  const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Form Demo</h1>
-      <Form form={form} initialValues={{}}>
+      <Form form={form} setForm={setForm}>
         <Field name="userId">
           <Input></Input>
         </Field>
         <Field name="password">
           <Input></Input>
         </Field>
-        <Field>
-          <button
-            onClick={() => {
-              console.log(form);
-            }}
-          >
-            submit
-          </button>
-        </Field>
+        <button
+          onClick={() => {
+            console.log(form);
+          }}
+        >
+          submit
+        </button>
       </Form>
+      <p>
+        {count}
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          add
+        </button>
+      </p>
+      <Test />
     </div>
   );
 };
+
+function Test() {
+  console.log("test");
+  return <p>test</p>;
+}
 
 const Input = () => {
   const value = useContext(FieldContext);
@@ -82,12 +98,18 @@ const Input = () => {
   );
 };
 
-const Field = memo(
+const Field = (props) => {
+  const form = useContext(FormContext);
+  // const setForm = useContext(SetFormContext);
+  return <FieldEntity {...props} form={form} />;
+};
+
+const FieldEntity = memo(
   (props) => {
-    const { name, children, form, setForm } = props;
+    const { name, children, form } = props;
     console.log("field render", name);
     // const form = useContext(FormContext);
-    // const setForm = useContext(SetFormContext);
+    const setForm = useContext(SetFormContext);
     const updateForm = useCallback((value) => {
       setForm((prev) => {
         return { ...prev, [name]: value };
@@ -109,3 +131,10 @@ const Field = memo(
   }
 );
 export default FormDemo;
+
+/**
+ * 1. 命中缓存
+ *  state -> props -> context
+ * 2. context
+ *  会击穿 React.memo
+ */
