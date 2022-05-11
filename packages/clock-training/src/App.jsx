c8c7b8d8e3e43e1368b0aec9a134fffa829809a9
jtr354 <jtr354@foxmail.com>
2022-05-11 10:51:13 +0800

    feat:clock end

--- packages/clock-training/src/App.jsx
+++ packages/clock-training/src/App.jsx
@@ -1,45 +1,66 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useReducer, useState } from "react";
+
+import Clock from "react-clock";
+import "react-clock/dist/Clock.css";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [clocks, dispatch] = useReducer(
+    () =>
+      new Array(5).fill(1).map((it) => {
+        return createRandomTime();
+      }),
+    []
+  );
+  const [count, setCount] = useState(0);
+  const [show, setShow] = useState(false);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+    <div>
+      <h1>
+        <span
+          onClick={() => {
+            const next = window.confirm(
+              count ? "确定进入下一题?" : "准备好了吗? 家弘?"
+            );
+            if (next) {
+              setCount(count + 1);
+              dispatch();
+              setShow(false);
+            }
+          }}
+        >
+          {!count ? "开始" : "下一题"}
+        </span>
+        {!!count && (
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              setShow(!show);
+            }}
+          >
+            {!show ? "查看答案" : "隐藏答案"}
           </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+        )}
+      </h1>
+      {!!count ? <h2>第{count}题</h2> : null}
+      <section className="clockWrapper">
+        {clocks.map((value) => (
+          <div>
+            <Clock key={value} value={value} />
+            {show && <p>{value}</p>}
+          </div>
+        ))}
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+function createRandomTime() {
+  const h = String((Math.random() * 24) >> 0);
+  const m = String((Math.random() * 60) >> 0);
+  const s = String((Math.random() * 60) >> 0);
+  return [h.padStart(2, "0"), m.padStart(2, "0"), s.padStart(2, "0")].join(":");
+}
+
+export default App;
