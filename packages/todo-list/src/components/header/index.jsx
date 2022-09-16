59c9d2f57a5faa3e92e74c3d5e98f1183c5456ac
jtr354 <jtr354@foxmail.com>
2022-09-16 09:33:44 +0800

    feat:todo-list:useLayoutEffect & useEffect

--- packages/todo-list/src/components/header/index.jsx
+++ packages/todo-list/src/components/header/index.jsx
@@ -1,10 +1,20 @@
+import { useEffect, useRef, useLayoutEffect } from "react";
 import "./styles.css";
 import logo from "../../logo.svg";
 const Header = () => {
-  console.log(Header.name, 'run')
+  // console.log(Header.name, 'run')
+  const ref = useRef();
+  useEffect(() => {
+    console.log("useEffect", ref.current);
+  }, []);
+
+  useLayoutEffect(() => {
+    // 同步执行，https://juejin.cn/post/6959886436526981150
+    console.log("useLayoutEffect", ref.current.getAttribute("class"));
+  }, []);
 
   return (
-    <header className="App-header">
+    <header ref={ref} className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <a
         className="App-link"
